1b63e1867cb1128fe69580f490a169b3b646bb94
Dendi-Dewa <denidewaqq1@gmail.com>
2022-09-29 10:14:16 +0700

    Bootstrap dendi tugas

---
+++ 5520119044_DendiDewaPurnama.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
+    <title>BOOTSTRAP-DENDI</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container" style="background:red">
+    <h1>Hello, Den !</h1>
+    <div class="row">
+        <div class="col-6 col-md-3" style="background:green">
+         Col-4
+        </div>
+        
+        <div class="col-6 col-md-3" style="background:blue">
+            Col-4
+        </div>
+       
+        <div class="col-6 col-md-3" style="background:yellow">
+            Col-4
+        </div>
+         
+        <div class="col-6 col-md-3" style="background:cyan">
+            Col-4
+        </div>
+
+
+    </div>
+</div>
+
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>Kolom 1</th>
+            <th>Kolom 2</th>
+            <th>Kolom 3</th>
+            <th>Kolom 4</th>
+            <th>Kolom 5</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Kolom</td>
+            <td>Kolom</td>
+            <td>Kolom</td>
+            <td>Kolom</td>
+            <td>Kolom</td>
+        </tr>
+        <tr>
+            <td>Kolom</td>
+            <td>Kolom</td>
+            <td>Kolom</td>
+            <td>Kolom</td>
+            <td>Kolom</td>
+        </tr>
+
+        <tr>
+            <td>Kolom</td>
+            <td>Kolom</td>
+            <td>Kolom</td>
+            <td>Kolom</td>
+            <td>Kolom</td>
+        </tr>
+
+        <tr>
+            <td>Kolom</td>
+            <td>Kolom</td>
+            <td>Kolom</td>
+            <td>Kolom</td>
+            <td>Kolom</td>
+        </tr>
+
+        <tr>
+            <td>Kolom</td>
+            <td>Kolom</td>
+            <td>Kolom</td>
+            <td>Kolom</td>
+            <td>Kolom</td>
+        </tr>
+
+
+    </tbody>
+
+</table>
+
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  
+  
+</form>
+<select class="form-control">
+<option>One</option>
+<option>Two</option>
+<option>Three</option>
+<option>Four</option>
+
+</select>
+    
+</body>
+</html>
